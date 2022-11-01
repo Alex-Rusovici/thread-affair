50b8de84de52fe1106f7e3dcb8b51901cac5507e
Alexandru Rusovici <alex.rusovici@yahoo.com>
2022-11-01 22:11:31 +0200

    minor
    minor

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>Hello Thread Affair <i class="fa-solid fa-house"></i> </div>
+  return <div>Hello Thread Affair <i className="fa-solid fa-house"></i> </div>
 }
