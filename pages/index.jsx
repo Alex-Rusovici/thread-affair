89397535325a4b95ffa601f557da2a3062dc6168
Alexandru Rusovici <alex.rusovici@yahoo.com>
2022-11-03 22:02:05 +0200

    art guide
    art guide

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,8 +2,13 @@ export default function Home() {
   return (
     <div>
       Hello Thread Affair <i className="fa-solid fa-house"></i>{' '}
-
-      <img src="/images/logo.jpg" alt="Alt" />
+      <img src="/images/logo.jpg" alt="Logo Thread Affair" />
+      <button className="button button-primary">Button Primary</button>
+      <button className="button button-primary button-large">
+        Button Primary Large
+      </button>
+      <a className="button button-primary">Button Primary</a>
+      <a className="button button-primary button-large">Button Primary Large</a>
     </div>
   );
 }
